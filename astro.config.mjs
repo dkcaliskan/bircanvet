f37915652aee514fd9fcc4cc57d3652ba0a83a83
dkcaliskan <dogukaan.caliskan@gmail.com>
2024-05-18 01:23:13 +0300

    sitemap & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 import { imageService } from '@unpic/astro/service'
-
 import netlify from '@astrojs/netlify'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind()],
+	site: 'https://bircanvet.com',
+	integrations: [svelte(), tailwind(), sitemap()],
 	image: {
 		service: imageService({
 			// This can usually be auto-detected
